e73a727b88f95cd4cfb6ed3d541bcf6fb0fe0d85
Junsoo Park <matthiaspark0514@gmail.com>
2024-07-29 13:25:36 -0700

    Feat: Test Helper Review

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,3 +1,18 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
 // updated imports added for convenience
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+const assert = require("assert");
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require("../compile");
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '1000000'});
+});
